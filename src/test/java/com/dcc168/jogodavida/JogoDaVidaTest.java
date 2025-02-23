c15df7726a8c97c5adda919f2d6f52bef6aac1a2
Davi Kirchmaier Paiva <104040768+KirchmaierDavi@users.noreply.github.com>
2025-02-23 15:31:28 -0300

    Problema com as dependencias resolvidos e pequenas mudanças e implementações nos testes

--- src/test/java/com/dcc168/jogodavida/JogoDaVidaTest.java
+++ src/test/java/com/dcc168/jogodavida/JogoDaVidaTest.java
@@ -1,12 +1,13 @@
 package com.dcc168.jogodavida;
 
+import static org.junit.Assert.*;
+
 import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 public class JogoDaVidaTest {
     private JogoDaVida jogo;
-   
+
     @Before
     public void setUp() {
         jogo = new JogoDaVida();
@@ -14,21 +15,55 @@ public class JogoDaVidaTest {
 
     @Test
     public void testContarVizinhosVivos() {
-        int[][] tabuleiro = {
-            {1, 1, 0, 0, 0, 0},
-            {1, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0}
-        };
+        int[][] tabuleiro = new int[6][6];
+        tabuleiro[1][1] = 1;
+        tabuleiro[1][2] = 1;
+        tabuleiro[2][1] = 1;
+        
         jogo.setTabuleiro(tabuleiro);
-        assertEquals(3, jogo.contarVizinhosVivos(0, 1));
+        assertEquals(2, jogo.contarVizinhosVivos(1, 1));
+    }
+
+    @Test
+    public void testProximaGeracao() {
+        int[][] tabuleiro = new int[6][6];
+        tabuleiro[1][1] = 1;
+        tabuleiro[1][2] = 1;
+        tabuleiro[2][1] = 1;
+        
+        jogo.setTabuleiro(tabuleiro);
+        jogo.proximaGeracao();
+        
+        int[][] resultado = jogo.getTabuleiro();
+        assertEquals(1, resultado[1][1]);
+        assertEquals(1, resultado[1][2]);
+        assertEquals(1, resultado[2][1]);
+        assertEquals(1, resultado[2][2]);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testSetTabuleiroTamanhoInvalido() {
+        jogo.setTabuleiro(new int[5][5]);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testContarVizinhosVivosForaDoLimite() {
         jogo.contarVizinhosVivos(-1, 0);
+    }
+
+    @Test
+    public void testAplicarRegras() {
+
+        assertEquals(0, jogo.aplicarRegras(1, 1));
+        
+        assertEquals(1, jogo.aplicarRegras(1, 2));
+        assertEquals(1, jogo.aplicarRegras(1, 3));
+        
+        assertEquals(0, jogo.aplicarRegras(1, 4));
+        
+        assertEquals(1, jogo.aplicarRegras(0, 3));
+        
+        assertEquals(0, jogo.aplicarRegras(0, 2));
     }
 
     @Test
@@ -89,5 +124,31 @@ public class JogoDaVidaTest {
         jogo.setTabuleiro(tabuleiro);
         jogo.proximaGeracao();
         assertEquals(1, jogo.getTabuleiro()[1][1]);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testSetTabuleiroInvalido() {
+        int[][] tabuleiroInvalido = {
+            {1, 1, 0, 0, 0},
+            {1, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0}
+        };
+        jogo.setTabuleiro(tabuleiroInvalido);
+    }
+
+    @Test
+    public void testSetTabuleiroValido() {
+        int[][] tabuleiroValido = {
+            {1, 1, 0, 0, 0, 0},
+            {1, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0}
+        };
+        jogo.setTabuleiro(tabuleiroValido);
+        assertArrayEquals(tabuleiroValido, jogo.getTabuleiro());
     }
 }
